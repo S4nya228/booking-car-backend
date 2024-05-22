7f47a950fce45ece623c98b7e4a2aa5e9ce4d441
Moreka Oleksandr <gyzica40@gmail.com>
2024-05-22 14:32:25 +0300

    add request for car view

---
+++ app/Http/Requests/Admin/Car/CarRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Admin\Car;
+
+use App\Enum\AWDEnum;
+use App\Enum\CarClassEnum;
+use App\Enum\EngineTypeEnum;
+use Illuminate\Validation\Rule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'car_class' => ['required', Rule::enum(CarClassEnum::class)],
+            'brand' => ['required', 'string'],
+            'color' => ['required', 'string'],
+            'engine_type' => ['required', Rule::enum(EngineTypeEnum::class)],
+            'engine_power' => ['required', 'integer'],
+            'wheel_drive' => ['required', Rule::enum(AWDEnum::class)],
+            'zero_to_full' => ['required', 'integer'],
+            'price' => ['required', 'string'],
+            'image_path.*' => ['required', 'image', 'max:8192', 'mimes:png,jpg,gif,jpeg,webp'],
+        ];
+    }
+}
