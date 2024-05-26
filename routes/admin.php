752ebb2e1f773efd8a3da3ad392d133c4a8da588
Moreka Oleksandr <gyzica40@gmail.com>
2024-05-26 23:46:31 +0300

    fix: put status to post

--- routes/admin.php
+++ routes/admin.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\Admin\BookingCar\BookingCarController;
 use App\Http\Controllers\Admin\Car\CarController;
+use App\Models\BookingCar;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('cars', CarController::class);
-Route::apiResource('booking', BookingCarController::class)->except('store');
-  
+Route::apiResource('booking', BookingCarController::class)->except(['store', 'update']);
+Route::post('booking/{booking}', [BookingCarController::class, 'update'])->name('update');
